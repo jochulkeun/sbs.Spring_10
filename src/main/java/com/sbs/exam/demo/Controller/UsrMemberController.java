7ff8d53cb440598a366426191b50b2c216683acd
kingdogbaby2 <kingdogbaby2@gmail.com>
2021-10-29 11:31:36 +0900

    Ut.empty 도입

--- src/main/java/com/sbs/exam/demo/Controller/UsrMemberController.java
+++ src/main/java/com/sbs/exam/demo/Controller/UsrMemberController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.sbs.exam.demo.Service.memberService;
+import com.sbs.exam.demo.util.Ut;
 import com.sbs.exam.demo.vo.Member;
  
 @Controller
@@ -19,22 +20,22 @@ public class UsrMemberController {
 	@RequestMapping("/usr/member/doJoin")
 	@ResponseBody
 	public Object doJoin(String loginId, String loginPw, String name, String nickname, String cellphoneNo, String email) {
-		if (loginId == null || loginId.trim().length() == 0) {
+		if (Ut.Empty(loginId)) {
 			return "loginId(을)를 입력해주세요.";
 		}
-		if (loginPw == null || loginPw.trim().length() == 0) {
+		if (Ut.Empty(loginPw)) {
 			return "loginPw(을)를 입력해주세요.";
 		}
-		if (name == null || name.trim().length() == 0) {
+		if (Ut.Empty(name)) {
 			return "name(을)를 입력해주세요.";
 		}
-		if (nickname == null || nickname.trim().length() == 0) {
+		if (Ut.Empty(nickname)) {
 			return "nickname(을)를 입력해주세요.";
 		}
-		if (cellphoneNo == null || cellphoneNo.trim().length() == 0) {
+		if (Ut.Empty(cellphoneNo)) {
 			return "cellphoneNo(을)를 입력해주세요.";
 		}
-		if (email == null || email.trim().length() == 0) {
+		if (Ut.Empty(email)) {
 			return "email(을)를 입력해주세요.";
 		}
 		

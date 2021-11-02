754a4eff61d333ea5fe92e4740591e6da2ce98ee
kingdogbaby2 <kingdogbaby2@gmail.com>
2021-11-02 14:06:08 +0900

    ResultData 구현 및 getArticle에 적용

---
+++ src/main/java/com/sbs/exam/demo/vo/ResultData.java
@@ -0,0 +1,35 @@
+package com.sbs.exam.demo.vo;
+
+import lombok.Getter;
+
+public class ResultData {
+	@Getter
+	private String resultCode;
+	@Getter
+	private String msg;
+	@Getter
+	Object data1;
+	
+	ResultData(){
+		
+	}
+	public static ResultData from(String resultCode,String msg) {
+		return ResultData.from(resultCode, msg, null);
+	}
+	
+	public static ResultData from(String resultCode,String msg,Object data1){
+		ResultData rd = new ResultData();
+		rd.resultCode = resultCode;
+		rd.msg = msg;
+		rd.data1 = data1;
+		return rd;
+		
+	}
+	
+	public boolean isSuccess() {
+		return resultCode.startsWith("S-");
+	}
+	public boolean isFail() {
+		return isSuccess()==false;
+	}
+}

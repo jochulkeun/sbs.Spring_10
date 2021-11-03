a84d693a2d4a9d43b78ee41d51f48bc147919aec
kingdogbaby2 <kingdogbaby2@gmail.com>
2021-11-03 15:36:07 +0900

    article/doDelete, article/doModify 에 ResultData 적용(제네릭 추가)

--- src/main/java/com/sbs/exam/demo/Controller/UsrArticleController.java
+++ src/main/java/com/sbs/exam/demo/Controller/UsrArticleController.java
@@ -16,24 +16,24 @@ import com.sbs.exam.demo.vo.ResultData;
 public class UsrArticleController {
 	@Autowired
 	private ArticleService articleService;
- 
+
 	// 액션 메서드 시작
 	@RequestMapping("/usr/article/doAdd")
 	@ResponseBody
 	public ResultData doAdd(String title, String body) {
-		if(Ut.Empty(title)) {
+		if (Ut.Empty(title)) {
 			return ResultData.from("F-1", "title을 입력해 주세요");
 		}
-		if(Ut.Empty(body)) {
+		if (Ut.Empty(body)) {
 			return ResultData.from("F-2", "body을 입력해 주세요");
 		}
-		
+
 		ResultData writeArticleRd = articleService.writeArticle(title, body);
-		int id = (int)writeArticleRd.getData1();
+		int id = (int) writeArticleRd.getData1();
 
 		Article article = articleService.getArticle(id);
 
-		return ResultData.newData(writeArticleRd,article);
+		return ResultData.newData(writeArticleRd, article);
 	}
 
 	@RequestMapping("/usr/article/getArticles")
@@ -48,38 +48,38 @@ public class UsrArticleController {
 		Article article = articleService.getArticle(id);
 
 		if (article == null) {
-			return ResultData.from("F-1",Ut.f("%d번 게시물은 존재하지 않습니다.",id) );
+			return ResultData.from("F-1", Ut.f("%d번 게시물은 존재하지 않습니다.", id));
 		}
 
-		return ResultData.from("S-1",Ut.f("%d번 게시물입니다", id), article);
+		return ResultData.from("S-1", Ut.f("%d번 게시물입니다", id), article);
 	}
 
 	@RequestMapping("/usr/article/doDelete")
 	@ResponseBody
-	public String doDelete(int id) {
+	public ResultData doDelete(int id) {
 		Article article = articleService.getArticle(id);
 
 		if (article == null) {
-			return id + "번 게시물은 존재하지 않습니다.";
+			return ResultData.from("F-1", Ut.f("%d번 게시물은 존재하지 않습니다.", id));
 		}
 
 		articleService.deleteArticle(id);
 
-		return id + "번 게시물을 삭제했습니다.";
+		return ResultData.from("S-1", Ut.f("%d번 게시물을 삭제했습니다.", id));
 	}
 
 	@RequestMapping("/usr/article/doModify")
 	@ResponseBody
-	public String doModify(int id, String title, String body) {
+	public ResultData doModify(int id, String title, String body) {
 		Article article = articleService.getArticle(id);
 
 		if (article == null) {
-			return id + "번 게시물은 존재하지 않습니다.";
+			return ResultData.from("F-1", Ut.f("%d번 게시물은 존재하지 않습니다.", id));
 		}
 
 		articleService.modifyArticle(id, title, body);
 
-		return id + "번 게시물을 수정했습니다";
+		return ResultData.from("F-1", Ut.f("%d번이 수정되었습니다.", id));
 	}
 
 	// 액션메서드 끝
